e4b060f3b6a44632dd1f651928bbda84f18b5c14
Ravi Prakash Srivastava <123312729+rpsrivastava24337@users.noreply.github.com>
2025-05-19 13:56:40 +0530

    ...

--- home.php
+++ home.php
@@ -215,7 +215,7 @@ $conn->close();
       display: flex;
       justify-content: center;
       align-items: center;
-      background: rgba(255, 255, 255, 0.35);
+      background: rgba(255, 255, 255, 0.7);
       /* Semi-transparent white */
       box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.10);
       border-radius: 18px;
@@ -236,7 +236,7 @@ $conn->close();
       display: flex;
       justify-content: center;
       align-items: center;
-      background: rgba(255, 255, 255, 0.35);
+      background: rgba(255, 255, 255, 0.7);
       /* Semi-transparent white */
       box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.10);
       border-radius: 18px;
